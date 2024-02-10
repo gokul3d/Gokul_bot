b214503de8caa6da27db684d14a2b8a1870ec2f6
gokul3d <gokuldevaraj4@gmail.com>
2024-02-09 23:03:17 -0500

    manylinks added

--- generate.py
+++ generate.py
@@ -1,6 +1,24 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")  # creates a new box
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
+# Initial dimensions of each block
+length = 1
+width = 1
+height = 1
 
-pyrosim.End()
+# Initial position (x, y, z)
+# The first block's bottom surface is at ground level
+x = 0
+y = 0
+z = height / 2  # Starting z-position to place the bottom of the first block at ground level
+
+pyrosim.Start_SDF("boxes.sdf")  # Start creating the SDF file
+
+# Loop to create and position each block
+for i in range(10):  # Iterates 10 times to create 10 blocks
+    # Send a cube with the current dimensions and position
+    pyrosim.Send_Cube(name=f"Box{i}", pos=[x, y, z], size=[length, width, height])
+
+    # Update z position for the next block to stack it directly on top of the current one
+    z += height  # Move z up by one block height for the next block
+
+pyrosim.End()  # Finish creating the SDF file
